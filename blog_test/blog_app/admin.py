7ab21b73ab272383105191788e1a9e60c4a8d934
nikita@mail.ru <nikita@mail.ru>
2024-03-20 17:40:43 +0100

    admin posts

--- blog_test/blog_app/admin.py
+++ blog_test/blog_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'created_on')
+
+admin.site.register(Post, PostAdmin)
