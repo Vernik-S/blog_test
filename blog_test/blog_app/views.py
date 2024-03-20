7efb65a5f5d1c272f63b78df44485d7454a9a72c
nikita@mail.ru <nikita@mail.ru>
2024-03-20 17:35:37 +0100

    post view

--- blog_test/blog_app/views.py
+++ blog_test/blog_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from blog_app.models import Post
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_app/index.html", context)
